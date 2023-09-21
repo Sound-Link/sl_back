d92c23c8ba96b8b6874ceb33d2ed5420b3295b07
이정민 <kyungheeyj45@ajou.ac.kr>
2023-09-21 22:04:04 +0900

    upload api create

--- domain/room/room_router.py
+++ domain/room/room_router.py
@@ -88,25 +88,27 @@ if not os.path.exists(AUDIO_PATH):
     os.makedirs(AUDIO_PATH)
 
 @router.post("/upload/")
-async def upload_audio(file: UploadFile = File(...)):
+async def upload_audio(file: UploadFile = None):
+    if not file:
+        raise HTTPException(status_code=400, detail="파일이 제공되지 않았습니다.")
+
     try:
-        # Read the audio file directly
+        # 오디오 파일을 직접 읽습니다.
         audio_bytes = file.file.read()
-        
-        # Recognize the audio data
-        recognizer = sr.Recognizer()
-        with sr.AudioFile(io.BytesIO(audio_bytes)) as source:
-            audio_data = recognizer.record(source)
-            text = recognizer.recognize_google(audio_data, language='ko-KR')
-        
+
+        # 오디오를 바로 인식합니다.
+        text = recognize_audio(audio_bytes)
         return {"status": "success", "text": text}
 
-    except sr.UnknownValueError:
-        raise HTTPException(status_code=400, detail="Google Web Speech API could not understand the audio.")
-    except sr.RequestError:
-        raise HTTPException(status_code=400, detail="Could not request results from Google Web Speech API.")
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error processing audio: {e}")
+        raise HTTPException(status_code=400, detail=str(e))
+
+def recognize_audio(audio_bytes: bytes) -> str:
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(io.BytesIO(audio_bytes)) as source:
+        audio_data = recognizer.record(source)
+        text = recognizer.recognize_google(audio_data, language='ko-KR')
+    return text
 
 @router.get("/download/{filename}/")
 async def download_audio(filename: str):
